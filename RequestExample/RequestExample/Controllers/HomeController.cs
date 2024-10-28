e7502d97f4d2d6e9db2f5525ac09eba302d4d48d
nagaraju onteru <nagaraju_onteru@epam.com>
2024-10-28 19:48:52 +0530

    Request and Response Example

---
+++ RequestExample/RequestExample/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RequestExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public void Index()
+        {
+            /*Response.Write("<p>URL: " + Request.Url.ToString() + "</p>");
+            Response.Write("<p>Path: " + Request.Path + "</p>");
+            Response.Write("<p>Query String: " + Request.QueryString + "</p>");
+            Response.Write("<p>Method: " + Request.HttpMethod + "</p>");
+            Response.Write("<p>Request headers: " + Request.Headers["User-Agent"] + "</p>");
+            Response.Write("<p>Browser: " + Request.Browser.Type + "</p>");
+            Response.Write("<p>Physical App Path: " + Request.PhysicalApplicationPath + "</p>");*/
+
+            Response.Write("Hello World");
+            Response.Headers.Add("X", "100");
+            Response.ContentType = "text/plain";
+            Response.StatusCode = 500;
+
+        }
+    }
+}
