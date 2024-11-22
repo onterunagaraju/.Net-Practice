71e65f7e59034b6a7dc27851f88103c98667f5a2
nagaraju onteru <nagaraju_onteru@epam.com>
2024-11-22 21:49:06 +0530

    Dto, Stopped at DTO

--- PokemonReviewApp/PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/PokemonReviewApp/Repository/PokemonRepository.cs
@@ -12,10 +12,35 @@ namespace PokemonReviewApp.Repository
             _context = context;
         }
 
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int id)
+        {
+            var review = _context.Reviews.Where(r => r.Pokemon.Id == id);
+            if (review.Count() < 0)
+                return 0;
+            return (decimal) review.Sum(r=> r.Rating) / review.Count();
+        }
 
         public ICollection<Pokemon> GetPokemons()
         {
             return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int id)
+        {
+            var pokemon = _context.Pokemon.Where(p => p.Id == id);
+            if(pokemon.Count() > 0)
+                return true;
+            return false;
         }
     }
 }
