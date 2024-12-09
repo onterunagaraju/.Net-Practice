f81681c23de6bd66f46918cb604edaeccfafc9ce
nagaraju onteru <nagaraju_onteru@epam.com>
2024-12-10 00:54:11 +0530

    Auto Mapper for Dto

--- PokemonReviewApp/PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/PokemonReviewApp/Controllers/PokemonController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
 
@@ -9,16 +11,19 @@ namespace PokemonReviewApp.Controllers
     public class PokemonController: Controller
     {
         private readonly IPokemonRepository _pokemonRepository;
-        public PokemonController(IPokemonRepository pokemonRepository)
+        private readonly IMapper _mapper;
+
+        public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
         {
             _pokemonRepository = pokemonRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
         public IActionResult GetPokemons()
         {
-            var pokemons = _pokemonRepository.GetPokemons();
+            var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
 
             if(!ModelState.IsValid)
                 return BadRequest(ModelState);
@@ -33,7 +38,7 @@ namespace PokemonReviewApp.Controllers
         {
             if (!_pokemonRepository.PokemonExists(pokeId))
                 return NotFound();
-            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+            var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(pokeId));
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
             return Ok(pokemon);
